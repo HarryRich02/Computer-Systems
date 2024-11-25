e67498746f0c672a299c2a67a29fa403c0cef6f6
HarryRich02 <klvr36@durham.ac.uk>
2024-11-25 15:52:03 +0000

    Assignment + Python Code

---
+++ LMC.py
@@ -0,0 +1,13 @@
+n = int(input())
+print(n)
+while True:
+    if n  == 1:
+        break
+    elif n > 999:
+        print(0)
+        break
+    elif n % 2 == 0:
+        n = n/2
+    elif n % 2 == 1:
+        n = (3*n + 1)/2
+    print(n)
